6e8a480c6918fab895a0dd5b6727ecc844f86cee
Pablosky992 <pnarciso92@gmail.com>
2026-06-29 21:23:41 +0200

    Delete scratch directory

--- scratch/get_end.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const filePath = path.join(__dirname, '../app.js');
-const content = fs.readFileSync(filePath, 'utf8');
-const lines = content.split('\n');
-
-console.log(`Total lines: ${lines.length}`);
-console.log(lines.slice(lines.length - 60).join('\n'));
